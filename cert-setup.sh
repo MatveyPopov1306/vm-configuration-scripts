ec31a275291b208c611e6f5c78e31d8e70cfdcd9
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-09-15 14:03:17 +0000

    creates a new branch and file cert-setup.sh

---
+++ cert-setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+clear
+
+# Shortcuts of colors
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+RESET='\e[0m'
+
+# Error codes for echo -e
+INFO="${BLUE}[INFO]${RESET}"
+OK="${GREEN}[OK]${RESET}"
+ERROR="${RED}[ERROR]${RESET}"
+WARNING="${YELLOW}[WARNING]${RESET}"
+
+
+
+main(){
+
+
+
+    return 0
+}
+
+main
