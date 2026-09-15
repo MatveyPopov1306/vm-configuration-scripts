5cc6eaa55696b6b740e5fd5919fbd0215285faba
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-09-15 14:18:19 +0000

    add system update func

--- cert-setup.sh
+++ cert-setup.sh
@@ -15,13 +15,44 @@ readonly OK="${GREEN}[OK]${RESET}"
 readonly ERROR="${RED}[ERROR]${RESET}"
 readonly WARNING="${YELLOW}[WARNING]${RESET}"
 
+# installation flags
+SKIPUPDATE=false
 
+# Parsing cycle main
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --skip-update)
+            SKIPUPDATE=true
+            shift
+            ;;
+        *)
+            echo "An unknown parameter was passed: $1"
+            echo -e "$ERROR installation was cancelled"
+            exit 1
+            ;;
+    esac
+done
+
+update_system() {
+
+	# Check if update is skipping
+	if [ "$SKIPUPDATE" = true ]; then
+		echo -e "$WARNING Skipped system update because of $SKIPUPDATE parameter --skip-update"
+		return 0
+	fi
+
+	# Update the System
+	sudo apt update
+	sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+	clear
+	echo -e "$OK System updated"
+
+}
 
 main(){
 
+    update_system
 
-
-    return 0
 }
 
 main
